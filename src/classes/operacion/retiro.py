b6b83906900c2d5f74199f2d6a886153b60cb0dd
Emmanuel Valentin <ema_110@outlook.es>
2023-09-10 00:58:47 -0600

    Add retiro.py

---
+++ src/classes/operacion/retiro.py
@@ -0,0 +1,7 @@
+from ..operacion import Operacion
+
+
+class Retiro(Operacion):
+    def realizar_operacion():
+        print("Realizando operacion desde Retiro")
+        # TODO: Realizar movimiento en la BD
