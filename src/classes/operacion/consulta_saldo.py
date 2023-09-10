d4fc4331ef7670da63c7db7f40f89651bce59f0b
Emmanuel Valentin <ema_110@outlook.es>
2023-09-10 00:58:01 -0600

    Add consultar_saldo.py

---
+++ src/classes/operacion/consulta_saldo.py
@@ -0,0 +1,7 @@
+from ..operacion import Operacion
+
+
+class Consulta_Saldo(Operacion):
+    def realizar_operacion():
+        print("Realizando operacion desde Actualizar_Saldo")
+        # TODO: Realizar movimiento en la BD
