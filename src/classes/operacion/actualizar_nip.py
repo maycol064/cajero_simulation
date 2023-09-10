a67dd83b67eea7db54f6a500b4d80b840df192ac
Emmanuel Valentin <ema_110@outlook.es>
2023-09-10 00:57:45 -0600

    Add actualizar_nip.py

---
+++ src/classes/operacion/actualizar_nip.py
@@ -0,0 +1,7 @@
+from ..operacion import Operacion
+
+
+class Actualizar_NIP(Operacion):
+    def realizar_operacion():
+        print("Realizando operacion desde Actualizar_NIP")
+        # TODO: Realizar movimiento en la BD
