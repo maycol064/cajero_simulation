8484fe2428c252a266898822c5e5d2daf7fe4c51
Emmanuel Valentin <ema_110@outlook.es>
2023-09-10 00:58:36 -0600

    Add pago.py

---
+++ src/classes/operacion/pago.py
@@ -0,0 +1,13 @@
+from ..operacion import Operacion
+
+
+class Pago(Operacion):
+    def __init__(self, id, cuenta, fecha, hora, destino, forma_pago):
+        # Cuenta funciona como cuenta origen
+        super().__init__(id, cuenta, fecha, hora)
+        self.__destino = destino
+        self.__forma_pago = forma_pago
+
+    def realizar_operacion():
+        print("Realizando operacion desde Pago...\n")
+        # TODO: Realizar movimiento en la BD
